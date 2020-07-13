2a7d05afdd0229067a509b055f27ea9902455fe0
Benson <solar.24cc@gmail.com>
2020-07-13 16:28:49 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblAPIUrl = new System.Windows.Forms.Label();
+            this.tbxAPIUrl = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.tbxResult = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.cbxTLSVersion = new System.Windows.Forms.ComboBox();
+            this.tbxProtocol = new System.Windows.Forms.Label();
+            this.lblTLSVersion = new System.Windows.Forms.Label();
+            this.lblNetVersion = new System.Windows.Forms.Label();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblAPIUrl
+            // 
+            this.lblAPIUrl.AutoSize = true;
+            this.lblAPIUrl.Location = new System.Drawing.Point(12, 9);
+            this.lblAPIUrl.Name = "lblAPIUrl";
+            this.lblAPIUrl.Size = new System.Drawing.Size(47, 12);
+            this.lblAPIUrl.TabIndex = 0;
+            this.lblAPIUrl.Text = "API位置";
+            // 
+            // tbxAPIUrl
+            // 
+            this.tbxAPIUrl.Location = new System.Drawing.Point(65, 6);
+            this.tbxAPIUrl.Name = "tbxAPIUrl";
+            this.tbxAPIUrl.Size = new System.Drawing.Size(746, 22);
+            this.tbxAPIUrl.TabIndex = 1;
+            this.tbxAPIUrl.Text = "https://api.telegram.org/";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(817, 6);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // tbxResult
+            // 
+            this.tbxResult.Location = new System.Drawing.Point(65, 62);
+            this.tbxResult.Multiline = true;
+            this.tbxResult.Name = "tbxResult";
+            this.tbxResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbxResult.Size = new System.Drawing.Size(746, 303);
+            this.tbxResult.TabIndex = 3;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 65);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(34, 12);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "Result";
+            // 
+            // cbxTLSVersion
+            // 
+            this.cbxTLSVersion.FormattingEnabled = true;
+            this.cbxTLSVersion.Items.AddRange(new object[] {
+            "Default",
+            "TLS1.1",
+            "TLS1.2",
+            "SSL3/TLS1.0/TLS1.1/TLS1.2"});
+            this.cbxTLSVersion.Location = new System.Drawing.Point(65, 34);
+            this.cbxTLSVersion.Name = "cbxTLSVersion";
+            this.cbxTLSVersion.Size = new System.Drawing.Size(278, 20);
+            this.cbxTLSVersion.TabIndex = 5;
+            this.cbxTLSVersion.Text = "Default";
+            // 
+            // tbxProtocol
+            // 
+            this.tbxProtocol.AutoSize = true;
+            this.tbxProtocol.Location = new System.Drawing.Point(12, 37);
+            this.tbxProtocol.Name = "tbxProtocol";
+            this.tbxProtocol.Size = new System.Drawing.Size(44, 12);
+            this.tbxProtocol.TabIndex = 6;
+            this.tbxProtocol.Text = "Protocol";
+            // 
+            // lblTLSVersion
+            // 
+            this.lblTLSVersion.AutoSize = true;
+            this.lblTLSVersion.Location = new System.Drawing.Point(63, 378);
+            this.lblTLSVersion.Name = "lblTLSVersion";
+            this.lblTLSVersion.Size = new System.Drawing.Size(141, 12);
+            this.lblTLSVersion.TabIndex = 7;
+            this.lblTLSVersion.Text = "Client Support TLS Version: ";
+            // 
+            // lblNetVersion
+            // 
+            this.lblNetVersion.AutoSize = true;
+            this.lblNetVersion.Location = new System.Drawing.Point(63, 401);
+            this.lblNetVersion.Name = "lblNetVersion";
+            this.lblNetVersion.Size = new System.Drawing.Size(125, 12);
+            this.lblNetVersion.TabIndex = 8;
+            this.lblNetVersion.Text = ".Net FrameWork Version:";
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Location = new System.Drawing.Point(63, 426);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(758, 36);
+            this.lblDesc.TabIndex = 9;
+            this.lblDesc.Text = resources.GetString("lblDesc.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 475);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.lblNetVersion);
+            this.Controls.Add(this.lblTLSVersion);
+            this.Controls.Add(this.tbxProtocol);
+            this.Controls.Add(this.cbxTLSVersion);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.tbxResult);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.tbxAPIUrl);
+            this.Controls.Add(this.lblAPIUrl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TLS Checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAPIUrl;
+        private System.Windows.Forms.TextBox tbxAPIUrl;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox tbxResult;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.ComboBox cbxTLSVersion;
+        private System.Windows.Forms.Label tbxProtocol;
+        private System.Windows.Forms.Label lblTLSVersion;
+        private System.Windows.Forms.Label lblNetVersion;
+        private System.Windows.Forms.Label lblDesc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Web;
+
+using System.Net.Security;
+using System.Reflection;
+using System.Security.Authentication;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.IO.Compression;
+
+/// <summary>
+/// 參考：
+/// https://stackoverflow.com/questions/28286086/default-securityprotocol-in-net-4-5
+/// https://blog.darkthread.net/blog/webclient-and-tls12/
+/// 
+/// test API:
+/// https://payment.ecpay.com.tw    tls1.1 , tls1.2
+/// https://api2.funpay.com.tw      tls1.0
+/// 
+/// Check TLS Version Usage: (maybe .net framework 4.6 or after)
+/// https://stackoverflow.com/questions/48589590/which-tls-version-was-negotiated
+///   --install this : https://www.nuget.org/packages/System.IO.Compression/4.3.0 
+/// </summary>
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblNetVersion.Text += System.Environment.Version.ToString();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string result = string.Empty;
+            string url = tbxAPIUrl.Text.Trim();
+            string msg_title = "FunpayModel.get_access_token_V2";
+            try
+            {
+                result = http_web_request_data_by_get_IgnoreServerCertificate(url);//回傳Content-Type : application/json
+                tbxResult.Text = result;
+            }
+            catch (Exception ex)
+            {
+                //寫入log
+                tbxResult.Text = ex.ToString();
+            }
+        }
+
+
+
+        /// <summary>
+        /// 使用HttpWebRequest 以GET方式取得資料 - By Sam
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="nc"></param>
+        /// <param name="urlencode">是否要將參數以UrlEncode編碼</param>
+        /// <returns></returns>
+        public string http_web_request_data_by_get_IgnoreServerCertificate(string url, System.Collections.Specialized.NameValueCollection nc = null, bool urlencode = false)
+        {
+            var parameters = new System.Text.StringBuilder();
+            string strCallTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            string mykey = string.Empty;
+            string myvalue = string.Empty;
+            try
+            {
+                //準備將NameValueCollection轉換成NameValue字串
+                if (nc != null)
+                {
+                    foreach (string key in nc.Keys)
+                    {
+                        mykey = (true == urlencode) ? HttpUtility.UrlEncode(key) : key;
+                        myvalue = (true == urlencode) ? HttpUtility.UrlEncode(nc[key]) : nc[key];
+                        parameters.AppendFormat("{0}={1}&", mykey, myvalue);
+                    }
+                    if (parameters.Length >= 1)
+                        parameters.Length -= 1; //去掉最後的「&」號
+                    //把完整網址兜出來
+                    url += "?" + parameters.ToString();
+                }
+
+
+                //ASP.NET解決停用SSL 3.0、TLS 1.0的方案
+                //For Net Framework 4.0  (必需安裝.net framework 4.5)
+                //參考：http://slashlook.com/articles_20180604.html
+
+                if (cbxTLSVersion.SelectedItem.ToString() == "TLS1.1")
+                {
+                    System.Net.ServicePointManager.SecurityProtocol = (System.Net.SecurityProtocolType)768;
+                }
+                else if (cbxTLSVersion.SelectedItem.ToString() == "TLS1.2")
+                {
+                    System.Net.ServicePointManager.SecurityProtocol = (System.Net.SecurityProtocolType)3072;
+                }
+                else if (cbxTLSVersion.SelectedItem.ToString() == "SSL3/TLS1.0/TLS1.1/TLS1.2")
+                {
+
+                    System.Net.ServicePointManager.SecurityProtocol =
+                        (System.Net.SecurityProtocolType)768 |
+                        (System.Net.SecurityProtocolType)3072 |
+                        SecurityProtocolType.Ssl3 |
+                        SecurityProtocolType.Tls;                    
+                }
+                else  //Default
+                {
+                    System.Net.ServicePointManager.SecurityProtocol =
+                        SecurityProtocolType.Ssl3 |
+                        SecurityProtocolType.Tls;
+                }
+
+
+                var securityProtocolVersion = System.Net.ServicePointManager.SecurityProtocol;
+                lblTLSVersion.Text = "Client Support TLS Version: " + securityProtocolVersion.ToString();
+                //.net Framework 4.5 才有支援 Tls 1.2
+                //設定 HTTPS 連線時，不要理會憑證的有效性問題，參考自：https://eric0806.blogspot.tw/2011/05/webrequest.html
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+                //建立WebRequest元件
+                System.Net.HttpWebRequest myRequest = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+
+                //lblDesc.Text = string.Format("The HttpHeaders are Name Value {0}", myRequest.Headers);
+
+                myRequest.Method = "GET";
+                //myRequest.ContentType = "application/json";
+                myRequest.Accept = "application/json";
+                //讀取資料
+                System.Net.HttpWebResponse wr = (System.Net.HttpWebResponse)myRequest.GetResponse();
+                System.IO.Stream resStream = wr.GetResponseStream();
+                System.IO.StreamReader sr = new System.IO.StreamReader(resStream);
+                System.Text.StringBuilder output = new System.Text.StringBuilder();
+                
+
+                //lblDesc.Text += string.Format("The Response Headers are Name Value {0}", wr.Headers);
+
+                
+                //What TLS Version used
+                SslProtocols ssp = ExtractSslProtocol(resStream);
+                lblTLSVersion.Text += "  Negotiate Protocols = " + ssp.ToString();
+
+                output.Append(sr.ReadToEnd());
+                sr.Close();
+                return output.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        //.net framework newer version
+        /*
+        //using System.Net.Security;
+        //using System.Reflection;
+        //using System.Security.Authentication;
+        //using System.Security.Cryptography;
+        //using System.Security.Cryptography.X509Certificates;
+        //using System.IO;
+        //using System.IO.Compression;         
+         */
+        private static SslProtocols ExtractSslProtocol(Stream stream)
+        {
+            if (stream == null) return SslProtocols.None;
+            //if (stream == null) return;
+
+            BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic;
+            Stream metaStream = stream;
+            if (stream.GetType().BaseType == typeof(GZipStream))
+            {
+                metaStream = (stream as GZipStream).BaseStream;
+            }
+            else if (stream.GetType().BaseType == typeof(DeflateStream))
+            {
+                metaStream = (stream as DeflateStream).BaseStream;
+            }
+
+            var connection = metaStream.GetType().GetProperty("Connection", bindingFlags).GetValue(metaStream, null);
+            if (!(bool)connection.GetType().GetProperty("UsingSecureStream", bindingFlags).GetValue(connection, null))
+            {
+                // Not a Https connection
+                return SslProtocols.None;
+            }
+            var tlsStream = connection.GetType().GetProperty("NetworkStream", bindingFlags).GetValue(connection, null);
+            var tlsState = tlsStream.GetType().GetField("m_Worker", bindingFlags).GetValue(tlsStream);
+
+            return (SslProtocols)tlsState.GetType().GetProperty("SslProtocol", bindingFlags).GetValue(tlsState, null);
+        }
+    }
+}
